25fa3ec6a939d4031163c3a9b2fe1d0e7781d0b0
dfangs <dfanggohans@gmail.com>
2022-04-24 16:29:53 -0400

    Implement DCT for 3 channels + add some QOL refactor

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import steganography as stego
 
 
 if __name__ == '__main__':
-    cover_img = Image.from_file('images/testimage1_512x384.jpg')
-    hidden_img = Image.from_file('images/testimage1_96x72.jpg')
+    cover_img = Image.from_file('images/mit_512x384.jpg')
+    hidden_img = Image.from_file('images/mit_64x48.jpg')
     message = b'I hate crypto'
 
     # (1) Hide text message inside cover image using LSB
@@ -22,10 +22,13 @@ if __name__ == '__main__':
     # recovered_img.show()
 
     # (3) Hide an (unencrypted) image inside cover image using DCT
-    stego_img = stego.hide(cover_img.to_grayscale(), hidden_img.to_bytes(), mode=stego.Mode.DCT_LSB)
+    stego_img = stego.hide(cover_img, hidden_img.to_bytes(), mode=stego.Mode.DCT_LSB)
     # stego_img.show()
+    cover_img.to_grayscale().save('output/mit_grayscale.jpg')
+    stego_img.save('output/mit_stego_img.jpg')
+
     recovered_data = stego.recover(stego_img, mode=stego.Mode.DCT_LSB)
     truncated_recovered_data = recovered_data[:len(hidden_img.to_bytes())]
-    # assert hidden_img.to_bytes() == truncated_recovered_data
     recovered_img = Image.from_bytes(truncated_recovered_data, hidden_img.shape)
-    recovered_img.show()
+    # recovered_img.show()
+    recovered_img.save('output/mit_recovered_dct.jpg')
--- steganography.py
+++ steganography.py
@@ -1,12 +1,11 @@
-import cv2
 import numpy as np
 import numpy.typing as npt
+import util
+import lsb
+from dct import dct, idct
 from enum import Enum, auto
 from image import Image, ImageArray
 from typing import Union
-
-
-DctArray = npt.NDArray[np.int32]
 
 class Mode(Enum):
     """Represents various mode of steganography supported by this package."""
@@ -32,11 +31,11 @@ def hide(cover_img: Image, data: bytes, mode: Mode, key: bytes = None) -> Image:
 
     """
     if mode == Mode.LSB:
-        return Image(_hide_lsb(cover_img.array, data, key))
+        return _hide_lsb(cover_img, data, key)
     elif mode == Mode.DCT_LSB:
-        return Image(_hide_dct_lsb(cover_img.array, data, key))
+        return _hide_dct_lsb(cover_img, data, key)
     elif mode == Mode.DCT_ADD and key:
-        return Image(_hide_dct_add(cover_img.array, data, key))
+        return _hide_dct_add(cover_img, data, key)
     else:
         raise ValueError('The given combination of mode and key is not supported')
 
@@ -53,96 +52,69 @@ def recover(stego_img: Image, mode: Mode, key: bytes = None) -> bytes:
         ValueError: if the given combination of mode and key is not supported.
     """
     if mode == Mode.LSB:
-        return _recover_lsb(stego_img.array, key)
+        return _recover_lsb(stego_img, key)
     elif mode == Mode.DCT_LSB:
-        return _recover_dct_lsb(stego_img.array, key)
+        return _recover_dct_lsb(stego_img, key)
     elif mode == Mode.DCT_ADD and key:
-        return _recover_dct_add(stego_img.array, key)
+        return _recover_dct_add(stego_img, key)
     else:
         raise ValueError('The given combination of mode and key is not supported')
 
 
 ### IMPLEMENTATION FUNCTIONS
 
-def _hide_lsb(cover_img: Union[ImageArray, DctArray], data: bytes, key: bytes) -> ImageArray:
-    flat_img = cover_img.flatten()
-    flat_img &= (~np.zeros_like(flat_img) ^ 1)  # Remove LSB
+def _hide_lsb(cover_img: Image, data: bytes, key: bytes) -> Image:
+    return Image(lsb.embed(cover_img.array, data, key))
 
-    if key:
-        pass
+def _recover_lsb(stego_img: Image, key: bytes) -> bytes:
+    return lsb.extract(stego_img.array, key)
+
+def _hide_dct_lsb(cover_img: Image, data: bytes, key: bytes) -> Image:
+    if len(cover_img.shape) == 3:
+        ycrcb_img = cover_img.to_ycrcb()
+
+        dct_array = util.combine_channels(
+            [dct(channel) for channel in util.separate_channels(ycrcb_img.array)]
+        )
+        stego_dct_array = lsb.embed(dct_array, data, key)
+
+        return Image(util.combine_channels(
+            [idct(channel) for channel in util.separate_channels(stego_dct_array)]
+        )).to_rgb()
+
     else:
-        # TODO: Check hiding capacity
-        # if 8*len(data) > len(flat_img):
-        #     raise ValueError('len(data) exceeds the hiding capacity')
+        dct_array = dct(cover_img.array)
+        stego_dct_array = lsb.embed(dct_array, data, key)
+        return Image(idct(stego_dct_array)).to_rgb()
 
-        data_bitarray = _bytes_to_bitarray(data)
-        flat_img[:len(data_bitarray)] |= data_bitarray
+def _recover_dct_lsb(stego_img: Image, key: bytes) -> bytes:
+    if len(stego_img.shape) == 3:
+        ycrcb_img = stego_img.to_ycrcb()
 
-    stego_img = np.reshape(flat_img, cover_img.shape)
-    return stego_img
-
-def _recover_lsb(stego_img: Union[ImageArray, DctArray], key: bytes) -> bytes:
-    flat_img = stego_img.flatten()
-    flat_img &= np.ones_like(flat_img)
-
-    if key:
-        pass
+        dct_array = util.combine_channels(
+            [dct(channel) for channel in util.separate_channels(ycrcb_img.array)]
+        )
     else:
-        data = _bitarray_to_bytes(flat_img)
+        dct_array = dct(stego_img.array)
 
-    return data
+    return lsb.extract(dct_array, key)
 
-def _hide_dct_lsb(cover_img: ImageArray, data: bytes, key: bytes) -> ImageArray:
-    dct_img = _dct(cover_img)
-    stego_dct_img = _hide_lsb(dct_img, data, key)
-    return _idct(stego_dct_img)
-
-def _recover_dct_lsb(stego_img: ImageArray, key: bytes) -> bytes:
-    dct_img = _dct(stego_img)
-    return _recover_lsb(dct_img, key)
-
-def _hide_dct_add(cover_img: ImageArray, data: bytes, key: bytes) -> ImageArray:
+def _hide_dct_add(cover_img: Image, data: bytes, key: bytes) -> Image:
     assert key
 
-    dct_img = _dct(cover_img)
+    dct_img = dct(cover_img)
     flat_dct_img = dct_img.flatten()
 
     # TODO: Fix
-    data_bitarray = _bytes_to_bitarray(data)
+    data_bitarray = util.bytes_to_bitarray(data)
     flat_dct_img[:len(data_bitarray)] += data_bitarray
 
     stego_img = np.reshape(flat_dct_img, cover_img.shape)
-    return _idct(stego_img)
+    return idct(stego_img)
 
-def _recover_dct_add(stego_img: ImageArray, key: bytes) -> bytes:
+def _recover_dct_add(stego_img: Image, key: bytes) -> bytes:
     assert key
 
     # TODO: Fix
-    dct_img = _dct(stego_img)
+    dct_img = dct(stego_img)
     return _recover_lsb(dct_img)
-
-
-### HELPER FUNCTIONS
-
-def _bytes_to_bitarray(data: bytes) -> npt.NDArray[np.uint8]:
-    bitstring = ''.join('{0:08b}'.format(byte) for byte in data)  # Separate individual bits
-    bitarray = np.array(list(bitstring), dtype=np.uint8)
-    return bitarray
-
-def _bitarray_to_bytes(bitarray: npt.NDArray[np.uint8]) -> bytes:
-    bitstring = ''.join(bitarray.astype(str))
-    return bytes([int(bitstring[8*i : 8*(i+1)], 2) for i in range(len(bitstring) // 8)])
-
-def _dct(img: ImageArray) -> DctArray:
-    if len(img.shape) != 2:
-        raise ValueError('Image is not grayscale')
-
-    dct_float = cv2.dct(img.astype(np.float32))
-    return np.rint(dct_float).astype(np.int32)
-
-def _idct(img: DctArray) -> ImageArray:
-    if len(img.shape) != 2:
-        raise ValueError('Image is not grayscale')
-
-    idct_float = cv2.idct(img.astype(np.float32))
-    return np.rint(idct_float).astype(np.uint8)
