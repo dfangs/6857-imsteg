8a2a30fb2668fd516ac48f1f57dc41c903772ec4
cljin <cljin@mit.edu>
2022-05-09 14:52:28 -0400

    fixed plot_metrics to get plots

--- plot_metrics.py
+++ plot_metrics.py
@@ -10,7 +10,7 @@ import steganography as stego
 import metrics as metriclib
 from metrics import Metric
 
-def plot_psnr_graph(cover_img: cv2.Mat, full_secret: bytes, file_to_save: str, hidden_capacity_perc: int = 2) -> None:
+def plot_psnr_graph(cover_img: cv2.Mat, full_secret: bytes, file_to_save: str, hidden_capacity_perc: int = 5) -> None:
     """
     Save psnr plot for given cover_img for different hiding
     if cover_img is not grayscale, will be using grayscale
@@ -27,13 +27,13 @@ def plot_psnr_graph(cover_img: cv2.Mat, full_secret: bytes, file_to_save: str, h
     x = []
 
     
-    for hidden_capacity in range(1, size, size//perc_increment):
+    for hidden_capacity in range(1, size, size*perc_increment//100):
         for stego_method in mode_to_psnr:
             secret = full_secret[:hidden_capacity]
             stego_img = stego.hide(Image(cover_img), secret, stego_method).matrix if hidden_capacity > 0 else cover_img
 
-            cv2.imwrite(f"{stego_method.name}_{hidden_capacity/size * 100}.png", stego_img)
-
+            # cv2.imwrite(f"{stego_method.name}_{hidden_capacity/size * 100}.png", stego_img)
+            print(hidden_capacity, size)
             mode_to_psnr[stego_method].append(metriclib.get_psnr(cover_img, stego_img))
         x.append(hidden_capacity)
 
@@ -50,7 +50,7 @@ def plot_psnr_graph(cover_img: cv2.Mat, full_secret: bytes, file_to_save: str, h
     plt.savefig(file_to_save)
 
 
-def plot_rs_graph(cover_img: cv2.Mat, full_secret: bytes, file_to_save: str, hidden_capacity_perc: int = 2, stego_method = None) -> None:
+def plot_rs_graph(cover_img: cv2.Mat, full_secret: bytes, file_to_save: str, hidden_capacity_perc: int = 5, stego_method = None) -> None:
     """
     Save rs plot for the given cover_img storing full_secret at different capacities
     if cover_img is not grayscale, will be using grayscale
@@ -68,7 +68,7 @@ def plot_rs_graph(cover_img: cv2.Mat, full_secret: bytes, file_to_save: str, hid
 
     
 
-    for hidden_capacity in range(0, size, size//perc_increment):
+    for hidden_capacity in range(0, size, size*perc_increment//100):
         secret = full_secret[:hidden_capacity]
         img = cover_img
         if stego_method:
